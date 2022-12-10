b17279e95cae2cddb2dc578ae70ce1397ec60796
flavian-anselmo <anselmoflavian4@gmail.com>
2022-12-10 09:27:22 +0300

    all req converted from raw sql to ORM based using SQL Alchemy and pydantic for the schema

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import  Depends, FastAPI, Response, status
+from fastapi import  Depends, FastAPI, HTTPException, Response, status
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from pydantic import BaseModel
@@ -59,45 +59,63 @@ async def get_posts(db:session = Depends(get_db)):
 
 
 @app.post("/post")
-async def create_post(post:Post):
-    cursor.execute(""" INSERT INTO posts (title, description, published) VALUES (%s, %s, %s) RETURNING * """,(post.title,post.description,post.published))
-    # return the post 
-    new_post = cursor.fetchone()
-    # commint the changes to the db 
-    conn.commit()
-    return {"data":new_post}
+async def create_post(post:Post, db:session = Depends(get_db)):
+
+    # cursor.execute(""" INSERT INTO posts (title, description, published) VALUES (%s, %s, %s) RETURNING * """,(post.title,post.description,post.published))
+    # # return the post 
+    # new_post = cursor.fetchone()
+    # # commint the changes to the db 
+    # conn.commit()
+
+   #new_post =  models.Post( title = post.title,description = post.description,published = post.published)
+   # commit 
+   new_post =  models.Post( **post.dict())
+
+   db.add(new_post)
+   db.commit()
+   db.refresh(new_post)
+   return {"data":new_post}
+
 
 
 
 
 @app.get("/posts/{postid}")
-async def get_one_post(postid:int):
+async def get_one_post(postid: int, db:session =  Depends(get_db)):
 
-    cursor.execute(""" SELECT * FROM posts WHERE postid = %s""",(str(postid))) 
-    post = cursor.fetchone()
-
-    return {"post":post}
-
+    # cursor.execute(""" SELECT * FROM posts WHERE postid = %s""",(str(postid))) 
+    # post = cursor.fetchone()
+   post = db.query(models.Post).filter(models.Post.postid == postid).first()
+   return {"data":post}
 
 
 
 @app.delete("/posts/{postid}",status_code=status.HTTP_204_NO_CONTENT)
-async def delette_post(postid:int):
-    cursor.execute(""" DELETE  FROM posts WHERE postid = %s RETURNING * """, (str(postid)))
+async def delette_post(postid:int, db:session =  Depends(get_db)):
+    # cursor.execute(""" DELETE  FROM posts WHERE postid = %s RETURNING * """, (str(postid)))
 
-    cursor.fetchone()
-    conn.commit()
-
+    # cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.postid == postid)
+    post.first()
+    post.delete(synchronize_session = False)
+    db.commit()    
     return {"date":f"deleted item with index {postid}sucessfully "}
 
 
 
 
 @app.put("/posts/{postid}")
-async def update_post(postid:int,post:Post ):
+async def update_post(postid: int, post_update: Post, db: session = Depends(get_db) ):
+    # cursor.execute(""" UPDATE posts SET title = %s, description = %s, published = %s WHERE postid = %s  RETURNING * """, (post.title, post.description, post.published, str(postid)))
+    # updated = cursor.fetchone()
+    # conn.commit()
 
-    cursor.execute(""" UPDATE posts SET title = %s, description = %s, published = %s WHERE postid = %s  RETURNING * """, (post.title, post.description, post.published, str(postid)))
-    updated = cursor.fetchone()
-    conn.commit()
-    return {"data":updated}
+    post_query = db.query(models.Post).filter(models.Post.postid == postid)
+    post = post_query.first()
+    if post == None:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,detail="post not found")
+    post_query.update(post_update.dict(), synchronize_session = False)
+    db.commit()
+    return {"data": post_query.first()}
 
