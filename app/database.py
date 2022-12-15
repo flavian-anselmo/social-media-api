078d8e4f4bea35773fcea980e4e7b3e3137c01b2
flavian-anselmo <anselmoflavian4@gmail.com>
2022-12-15 21:12:32 +0300

    environment variables for db and jwt

--- app/database.py
+++ app/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.config import settings
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:anselmo@localhost/social-media-api'
+
+
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_host}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
