8021072de00416a78602332fe02cf896f8d0e919
flavian-anselmo <anselmoflavian4@gmail.com>
2022-12-17 01:23:13 +0300

    alembic databse migrations alembic upgrade head and alembic revision --autogenerate -m message

---
+++ alembic/versions/00664a857b32_create_table.py
@@ -0,0 +1,24 @@
+"""create table 
+
+Revision ID: 00664a857b32
+Revises: 
+Create Date: 2022-12-17 00:37:05.084273
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '00664a857b32'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
